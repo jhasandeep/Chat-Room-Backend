c12d2fde28920851f0f97c0187f9b1c3e9f4004a
Sandeep  kumar jha <121882218+jhasandeep@users.noreply.github.com>
2023-12-05 15:46:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 
 const io= new Server(server, {
     cors: {
-        origin : "http://localhost:3000",
+        origin : "https://grand-begonia-247564.netlify.app",
         methods: ["GET", "POST"]
     }
 })
