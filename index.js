1f0ec501671c58807537ce83b711d21164efcd06
Sandeep  kumar jha <121882218+jhasandeep@users.noreply.github.com>
2024-01-23 11:10:59 +0530

    Update index.js
    new

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const io= new Server(server, {
 })
 
 io.on("connection", (socket)=>{
-    console.log(socket.id);
+    // console.log(socket.id);
 
 
     socket.on("joinRoom" , room=>socket.join(room));
